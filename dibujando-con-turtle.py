ce19546337f5b043e885d48aef5d244f94b47127
Luis Alejandro <luisalejandro1234ale@gmail.com>
2017-10-02 16:53:52 -0400

    Update dibujando-con-turtle.py

--- dibujando-con-turtle.py
+++ dibujando-con-turtle.py
@@ -1,16 +1,13 @@
 import turtle
 
-def dibujar_cuadrado():
-  for x in range(0, 4):
-	  turtle.forward(50)
-	  turtle.left(90)
-  
-def mas_cuadrados():
-  #Más cuadrados
-  grados = 0
-  turtle.speed(15)
-  for x in range(1, 40):
-	  for x in range(0, 4):
-		  turtle.forward(50)
-		  turtle.left(90)
-	  turtle.left(grados + 10)
+luis = turtle.Turtle()
+luis.shape('turtle')
+luis.color('green')
+luis.forward(100)
+
+angel = turtle.Turtle()
+angel.shape('turtle')
+angel.color('blue')
+angel.right(180)
+angel.forward(100)
+ 

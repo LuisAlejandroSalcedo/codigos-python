730e5e63fec4fc89977ed77d1f8ba8a625b1eb54
Luis Alejandro <luisalejandro1234ale@gmail.com>
2017-10-16 18:10:04 -0400

    Add files via upload

---
+++ Plasmando respuestas en una tabla PDF.py
@@ -0,0 +1,43 @@
+import requests
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4, inch, landscape
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+
+archivo_pdf = SimpleDocTemplate("response_servers.pdf", pagesize=A4, rightMargin=30,leftMargin=30, topMargin=30,bottomMargin=18)
+archivo_pdf.pagesize = landscape(A4)
+elements = []
+urls = ["https://www.facebook.com", "https://es.wikipedia.org", "http://www.pythondiario.com/"]
+data = [["Dominio", "Codigo de Estatus", "Headers"]]
+
+
+ 
+#TODO: Get this line right instead of just copying it from the docs
+style = TableStyle([('ALIGN',(1,1),(-2,-2),'RIGHT'),
+                       ('TEXTCOLOR',(1,1),(-2,-2),colors.red),
+                       ('VALIGN',(0,0),(0,-1),'TOP'),
+                       ('TEXTCOLOR',(0,0),(0,-1),colors.blue),
+                       ('ALIGN',(0,-1),(-1,-1),'CENTER'),
+                       ('VALIGN',(0,-1),(-1,-1),'MIDDLE'),
+                       ('TEXTCOLOR',(0,-1),(-1,-1),colors.green),
+                       ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                       ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                       ])
+
+for links in urls:
+	response = requests.get(links)
+	rc = response.headers
+	data.append([str(links), str(response.status_code), str(rc)])
+
+#Configure style and word wrap
+s = getSampleStyleSheet()
+s = s["BodyText"]
+s.wordWrap = 'CJK'
+data2 = [[Paragraph(cell, s) for cell in row] for row in data]
+t=Table(data2)
+t.setStyle(style)
+ 
+#Send the data and build the file
+elements.append(t)
+archivo_pdf.build(elements)
+

3eebe4d4104f25eabc677ecea538d18a38961a96
Luis Alejandro <luisalejandro1234ale@gmail.com>
2017-11-14 20:57:41 -0400

    Create Ejemplo simple de Mechanize.py

---
+++ Ejemplo simple de Mechanize.py
@@ -0,0 +1,10 @@
+import mechanize
+
+br = mechanize.Browser()
+br.open("http://www.google.com")
+
+for form in br.forms():
+	form.find_control("hl").readonly = False
+	br.select_form(nr=0)
+	br.form["hl"] = "hhhhh"
+	print form
